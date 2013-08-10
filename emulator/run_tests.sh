7603ac7f1416f3398418e058ebcc4dfe203ca713
chrisu de <chrisudeussen@gmail.com>
2013-08-10 21:22:22 +0200

    add timeout to test script

--- emulator/run_tests.sh
+++ emulator/run_tests.sh
@@ -4,16 +4,15 @@ TESTS=`ls $TEST_DIR`
 for files in $TESTS
 do
 	touch emu.out
-	build/emu.elf $TEST_DIR/$files/$files.bin > emu.out
+	timeout 5s build/emu.elf $TEST_DIR/$files/$files.bin > emu.out
 	if diff emu.out $TEST_DIR/$files/$files.out > /dev/null; 
 	then
+		echo "+test $files passed"
 		rm emu.out
 	else
-		echo "test $files failed. output was:"
+		echo "-test $files failed"
+		echo "	output:"
 		cat emu.out
-		
-		echo "expected:"
-		cat $TEST_DIR/$files/$files.out
 	fi
 done
 
