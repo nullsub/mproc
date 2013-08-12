022c7a51dc3514acf6e066e1011539df7816e1d5
chrisu de <chrisudeussen@gmail.com>
2013-08-12 12:15:31 +0200

    fix math test. now 16bit add and subtract work

--- assembler/tests/math/math.asm
+++ assembler/tests/math/math.asm
@@ -4,30 +4,31 @@
 ;Add two 16bit integers
 	MOV	reg0, LOW(500)
 	MOV	reg1, HIGH(500)
-	PUSH	LOW(30)
-	PUSH	HIGH(30)
+	PUSH	LOW(1030)
+	PUSH	HIGH(1030)
 	MOV	reg3, HIGH(add16)
 	CALL	reg3, LOW(add16)
 
 ;Print result
-	MOV	reg3, LOW(itoa16)
-	CALL	reg3, HIGH(itoa16)
+	MOV	reg3, HIGH(itoa16)
+	CALL	reg3, LOW(itoa16)
 	MOV	reg3, HIGH(uart_print)
 	CALL	reg3, LOW(uart_print)
 
 ;subtract two 16bit integers
-	MOV	reg0, LOW(500)
-	MOV	reg1, HIGH(500)
-	;PUSH	LOW(30)
-	;PUSH	HIGH(30)
-	;MOV	reg3, HIGH(add16)
-	;CALL	reg3, LOW(add16)
+	MOV	reg0, LOW(15000)
+	MOV	reg1, HIGH(15000)
+	PUSH	LOW(10100)
+	PUSH	HIGH(10100)
+	MOV	reg3, HIGH(sub16)
+	CALL	reg3, LOW(sub16)
 
 ;Print result
-	MOV	reg3, LOW(itoa16)
-	;CALL	reg3, HIGH(itoa16)
+	MOV	reg3, HIGH(itoa16)
+	CALL	reg3, LOW(itoa16)
 	MOV	reg3, HIGH(uart_print)
-	;CALL	reg3, LOW(uart_print)
+	CALL	reg3, LOW(uart_print)
+;Print result
 
 	;halt the emulator
 	MOV	reg2, 0
@@ -49,10 +50,9 @@ add16:
 	SUB	reg1, 1
 add16_2:
 	ADD	reg1, 1
-add_16_end:					
 	RET
 
-;subtract 16bit numbers. result is 16 bit reg0_reg1 - reg2_reg3 = reg0_reg1
+;subtract 16bit numbers. result is 16 bit reg1_reg0 - reg3_reg2 = reg1_reg0
 ;reg0 low(nr1)
 ;reg1 high(nr1)
 ;stack1 low(nr2) -->reg2
@@ -61,22 +61,12 @@ add_16_end:
 sub16:
 	POP	reg3
 	POP	reg2
-	PUSH	reg1
-	MOV	reg1, reg0
-	SUB	reg1, reg2; sub LOW
+	SUB	reg0, reg2; sub LOW
 	JMPC	sub16_addc
 	JMP	sub16_continue
 sub16_addc:					
-	PUSH	reg3
-	MOV		reg3, reg2
-	NOR		reg3, reg2	; NOT the number
-	SUB	reg0, reg3
-	MOV	reg1, reg0
-	POP	reg3
-	ADD	reg3, 1; add the borrowed number
+	ADD	reg3, 1; add the borrowed numberumber
 sub16_continue:
-	MOV	reg0, reg1
-	POP	reg1
 	SUB	reg1, reg3; sub HIGH
 	RET
 
@@ -110,37 +100,36 @@ itoa:
 	MOV	reg2, 0x30; ASCII 0
 itoa_100_loop:
 	MOV	reg1, reg0
-	SUB		reg0, 100 ;if > 100
-	JMPC		itoa_end_100_loop
-	ADD		reg2, 1
-	JMP		itoa_100_loop
+	SUB	reg0, 100 ;if > 100
+	JMPC	itoa_end_100_loop
+	ADD	reg2, 1
+	JMP	itoa_100_loop
 itoa_end_100_loop:
-	MOV		reg0, reg1
-	STR		reg2
+	MOV	reg0, reg1
+	STR	reg2
 	PTR_ADD 1
 
-	MOV		reg2, 0x30 ;ASCII 0
+	MOV	reg2, 0x30 ;ASCII 0
 itoa_10_loop:
-	MOV		reg1, reg0
-	SUB		reg0, 10 
-	JMPC		itoa_end_10_loop
-	ADD		reg2, 1
-	JMP		itoa_10_loop
+	MOV	reg1, reg0
+	SUB	reg0, 10 
+	JMPC	itoa_end_10_loop
+	ADD	reg2, 1
+	JMP	itoa_10_loop
 itoa_end_10_loop:
-	MOV		reg0, reg1
-	STR		reg2
+	STR	reg2
 	PTR_ADD 1
 	;add the rest
-	ADD		reg0, 0x30
-	STR		reg0
+	ADD	reg1, 0x30
+	STR	reg1
 	PTR_ADD	1
-	;en
-	MOV		reg2, 0x0A
-	STR		reg2
+	;end
+	MOV	reg2, 0x0A
+	STR	reg2
 	PTR_ADD	1
-	MOV		reg2, 0x00
-	STR		reg2
-	MOV		reg0, HIGH(ITOA_MEM)
+	MOV	reg2, 0x00
+	STR	reg2
+	MOV	reg0, HIGH(ITOA_MEM)
 	MOV	reg1, LOW(ITOA_MEM)
 	RET
 
@@ -149,46 +138,74 @@ itoa_end_10_loop:
 ;reg1 high byte of number
 ;returns STRING ptr in reg0, reg1
 itoa16:
-	MOV		reg2, HIGH(ITOA_MEM)
+	PUSH	lr_low
+	PUSH	lr_high
+	MOV	reg2, HIGH(ITOA_MEM)
 	SET_PTR	reg2, LOW(ITOA_MEM)
-	MOV		reg2, 0x30; ASCII 0
-itoa_16_1000_loop:
-;	PUSH	LOW(1000)
-;	PUSH	HIGH(1000)
-;	MOV	reg2, LOW(sub16)
-;	CALL	reg2, HIGH(
-
-itoa16_100_loop:
-	MOV		reg1, reg0
-	SUB		reg0, 100 ;if > 100
-	JMPC		itoa16_end_100_loop; no carry detected
-	ADD		reg2, 1
-	JMP		itoa16_100_loop
-itoa16_end_100_loop:
-	MOV		reg0, reg1
-	STR		reg2
+	MOV	reg2, 0x30; ASCII 0 
+itoa16_1000_loop:
+	PUSH 	reg2
+	PUSH	reg1
+	PUSH	reg0
+	PUSH	LOW(1000)
+	PUSH	HIGH(1000)
+	MOV	reg2, HIGH(sub16)
+	CALL	reg2, LOW(sub16)
+	JMPC	itoa16_end_1000_loop
+	POP	reg2
+	POP	reg2
+	POP	reg2
+	ADD	reg2, 1
+	JMP	itoa16_1000_loop
+itoa16_end_1000_loop:
+	POP	reg0
+	POP	reg1
+	POP	reg2
+	STR	reg2
 	PTR_ADD 1
-	MOV		reg2, 0x30 ;ASCII 0
+	MOV	reg2, 0x30; ASCII 0
+itoa16_100_loop:
+	PUSH 	reg2
+	PUSH	reg1
+	PUSH	reg0
+	PUSH	LOW(100)
+	PUSH	HIGH(100)
+	MOV	reg2, HIGH(sub16)
+	CALL	reg2, LOW(sub16)
+	JMPC	itoa16_end_100_loop
+	POP	reg2
+	POP	reg2
+	POP	reg2
+	ADD	reg2, 1
+	JMP	itoa16_100_loop
+itoa16_end_100_loop:
+	POP	reg0
+	POP	reg1
+	POP	reg2
+	STR	reg2
+	PTR_ADD 1
+	MOV	reg2, 0x30; ASCII 0
 itoa16_10_loop:
-	MOV		reg1, reg0
-	SUB		reg0, 10 
-	JMPC		itoa16_end_10_loop; no carry detected
-	ADD		reg2, 1
-	JMP		itoa16_10_loop
+	MOV	reg1, reg0
+	SUB	reg0, 10 
+	JMPC	itoa16_end_10_loop; no carry detected
+	ADD	reg2, 1
+	JMP	itoa16_10_loop
 itoa16_end_10_loop:
-	MOV		reg0, reg1
-	STR		reg2
+	STR	reg2
 	PTR_ADD 1
 	;add the rest
-	ADD		reg0, 0x30
-	STR		reg0
+	ADD	reg1, 0x30
+	STR	reg1
 	PTR_ADD	1
 	;end
-	MOV					reg2, 0x0A
+	MOV	reg2, 0x0A
 	STR	reg2
 	PTR_ADD	1
-	MOV		reg2, 0x00
+	MOV	reg2, 0x00
 	STR	reg2
-	MOV		reg0, HIGH(ITOA_MEM)
+	MOV	reg0, HIGH(ITOA_MEM)
 	MOV	reg1, LOW(ITOA_MEM)
+	POP	lr_high
+	POP	lr_low
 	RET
